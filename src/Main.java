05540fb2ba0b41abded28d14a2a27c6d00a4f72d
Tetre2 <mattias.retteli@gmail.com>
2017-11-08 13:06:28 +0100

    här har vi en enkel appliation

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("TEMPLET");  //TODO
+		primaryStage.setTitle("Circle");  //TODO
 
 
 
