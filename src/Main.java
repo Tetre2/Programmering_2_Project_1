8d7a4524dc2a6a629c1045bfec5a90cb11a86cd8
Tetre2 <33448353+Tetre2@users.noreply.github.com>
2017-11-08 11:34:13 +0100

    Created main class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
+	import javafx.application.Application;
+	import javafx.scene.Group;
+	import javafx.scene.Scene;
+	import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
+	public static double worldWidth = 500;
+	public static double worldHeight = 500;
 
-}
+	
+		Group root = new Group();
+		Scene scene = new Scene(root, worldWidth, worldHeight);
+		
+		@Override
+		public void start(Stage primaryStage) throws Exception {
+			primaryStage.setTitle("TEMPLET");  //TODO
+			
+			
+			
+			
+		primaryStage.setScene(scene);	
+		primaryStage.show();	
+		
+		
+		}
+		
+		public static void main(String[] args) {
+			launch(args);
+		}
+		
+	}
